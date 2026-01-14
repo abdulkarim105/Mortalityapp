3a9905ac4e93f74be7fab8d3d4c8e30f884d0f01
abdulkarim105 <abdulkarimcuet12@gmail.com>
2026-01-14 23:01:09 +0100

    Update

--- hospital_ai/settings.py
+++ hospital_ai/settings.py
@@ -84,7 +84,7 @@ TIME_ZONE = "Europe/Berlin"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
