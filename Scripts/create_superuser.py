c711f5348a94fcb02c13a46ff0e432a48717661e
abdulkarim105 <abdulkarimcuet12@gmail.com>
2026-01-14 21:08:14 +0100

    Update

--- Scripts/create_superuser.py
+++ Scripts/create_superuser.py
@@ -1,20 +1,31 @@
-import os
-import django
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hospital_ai.settings")
-django.setup()
-
 from django.contrib.auth import get_user_model
-
 User = get_user_model()
 
 username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "datascience")
 email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "rihad@example.com")
 password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "quantumsix")
 
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
+# Try to find by username first
+user = User.objects.filter(username=username).first()
+
+# If not found, try to find old superuser and rename it
+if not user:
+    user = User.objects.filter(is_superuser=True).first()
+    if user:
+        user.username = username
+        print(f"Renaming superuser to {username}")
+
+if not user:
+    user = User.objects.create_superuser(
+        username=username,
+        email=email,
+        password=password,
+    )
     print("Superuser created")
 else:
-    print("Superuser already exists")
-
+    user.email = email
+    user.set_password(password)
+    user.is_staff = True
+    user.is_superuser = True
+    user.save()
+    print("Superuser updated")
